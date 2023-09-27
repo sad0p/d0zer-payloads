4c28c6b4ebe9776f4daefb96fbac6bda79c8924d
sad0p <sad0p@protonmail.com>
2023-09-27 18:10:49 -0400

    Updated shellcode.py to give c-style hex array and documentation in example-test.c payload

--- example-test.c
+++ example-test.c
@@ -23,11 +23,14 @@ Program Headers:
 196+0 records in
 196+0 records out
 196 bytes copied, 0.000648461 s, 302 kB/s
-[sad0p@arch-deliberate d0zer-payloads]$ ./shellcode.py payload.bin
-"b'b8 00 00 00 00 e8 08 00 00 00 e9 b2 00 00 00 90 0f 0b 55 48 89 e5 48 83 ec 20 48 b8 4a 75 73 74 20 61 20 74 48 ba 65 73 74 20 6d 61 6e 20 48 89 45 e0 48 89 55 e8 c7 45 f0 21 21 0a 00 48 8d 45 e0 48 89 c7 e8 17 00 00 00 48 89 c2 48 8d 45 e0 48 89 c6 bf 01 00 00 00 e8 33 00 00 00 90 c9 c3 55 48 89 e5 48 89 7d e8 48 c7 45 f8 00 00 00 00 eb 05 48 83 45 f8 01 48 8b 45 e8 48 8d 50 01 48 89 55 e8 0f b6 00 84 c0 75 e8 48 8b 45 f8 5d c3 55 48 89 e5 89 7d ec 48 89 75 e0 48 89 55 d8 48 c7 c0 01 00 00 00 48 8b 7d ec 48 8b 75 e0 48 8b 55 d8 0f 05 48 89 c0 48 89 45 f8 48 8b 45 f8 5d c3 90 0f 0b'
-"
-[sad0p@arch-deliberate d0zer-payloads]$ #ommit the last three bytes, copy and paste the rest into cyberchef with hex-to-hex translator with the second translator with \x delimitor
+[sad0p@arch-deliberate d0zer-payloads]$ ./shellcode.py 
+./shellcode.py <file-payload.bin> <optional-ommit-last-n-bytes>
+[sad0p@arch-deliberate d0zer-payloads]$ ./shellcode.py payload.bin 3
+"\xb8\x00\x00\x00\x00\xe8\x08\x00\x00\x00\xe9\xb2\x00\x00\x00\x90\x0f\x0b\x55\x48\x89\xe5\x48\x83\xec\x20\x48\xb8\x4a\x75\x73\x74\x20\x61\x20\x74\x48\xba\x65\x73\x74\x20\x6d\x61\x6e\x20\x48\x89\x45\xe0\x48\x89\x55\xe8\xc7\x45\xf0\x21\x21\x0a\x00\x48\x8d\x45\xe0\x48\x89\xc7\xe8\x17\x00\x00\x00\x48\x89\xc2\x48\x8d\x45\xe0\x48\x89\xc6\xbf\x01\x00\x00\x00\xe8\x33\x00\x00\x00\x90\xc9\xc3\x55\x48\x89\xe5\x48\x89\x7d\xe8\x48\xc7\x45\xf8\x00\x00\x00\x00\xeb\x05\x48\x83\x45\xf8\x01\x48\x8b\x45\xe8\x48\x8d\x50\x01\x48\x89\x55\xe8\x0f\xb6\x00\x84\xc0\x75\xe8\x48\x8b\x45\xf8\x5d\xc3\x55\x48\x89\xe5\x89\x7d\xec\x48\x89\x75\xe0\x48\x89\x55\xd8\x48\xc7\xc0\x01\x00\x00\x00\x48\x8b\x7d\xec\x48\x8b\x75\xe0\x48\x8b\x55\xd8\x0f\x05\x48\x89\xc0\x48\x89\x45\xf8\x48\x8b\x45\xf8\x5d\xc3"
+[sad0p@arch-deliberate d0zer-payloads]$ #last three bytes are garbage generated from the empty end_code function
+[sad0p@arch-deliberate d0zer-payloads]$ #we ommit them from the output
 [sad0p@arch-deliberate d0zer-payloads]$ export DOZEREGG="\xb8\x00\x00\x00\x00\xe8\x08\x00\x00\x00\xe9\xb2\x00\x00\x00\x90\x0f\x0b\x55\x48\x89\xe5\x48\x83\xec\x20\x48\xb8\x4a\x75\x73\x74\x20\x61\x20\x74\x48\xba\x65\x73\x74\x20\x6d\x61\x6e\x20\x48\x89\x45\xe0\x48\x89\x55\xe8\xc7\x45\xf0\x21\x21\x0a\x00\x48\x8d\x45\xe0\x48\x89\xc7\xe8\x17\x00\x00\x00\x48\x89\xc2\x48\x8d\x45\xe0\x48\x89\xc6\xbf\x01\x00\x00\x00\xe8\x33\x00\x00\x00\x90\xc9\xc3\x55\x48\x89\xe5\x48\x89\x7d\xe8\x48\xc7\x45\xf8\x00\x00\x00\x00\xeb\x05\x48\x83\x45\xf8\x01\x48\x8b\x45\xe8\x48\x8d\x50\x01\x48\x89\x55\xe8\x0f\xb6\x00\x84\xc0\x75\xe8\x48\x8b\x45\xf8\x5d\xc3\x55\x48\x89\xe5\x89\x7d\xec\x48\x89\x75\xe0\x48\x89\x55\xd8\x48\xc7\xc0\x01\x00\x00\x00\x48\x8b\x7d\xec\x48\x8b\x75\xe0\x48\x8b\x55\xd8\x0f\x05\x48\x89\xc0\x48\x89\x45\xf8\x48\x8b\x45\xf8\x5d\xc3"
+[sad0p@arch-deliberate d0zer-payloads]$
 [sad0p@arch-deliberate d0zer-payloads]$ ~/go/src/github.com/d0zer/d0zer -payloadEnv DOZEREGG -target ~/go/src/github.com/d0zer/experimental/helloworld -debug
 [+] Maximum payload size 0xe9f for /home/sad0p/go/src/github.com/d0zer/experimental/helloworld
 [+] Modifed entry point from 0x1040 to 0x1161
@@ -58,11 +61,11 @@ Program Headers:
 --------------------END------------------------------
 [+] Increased text segment p_filesz and p_memsz by 271 (length of payload)
 [+] Adjusting segments after text segment file offsets by 0x1000
-        Inceasing pHeader @ index 4 by 0x1000
-        Inceasing pHeader @ index 5 by 0x1000
-        Inceasing pHeader @ index 6 by 0x1000
-        Inceasing pHeader @ index 10 by 0x1000
-        Inceasing pHeader @ index 12 by 0x1000
+	Inceasing pHeader @ index 4 by 0x1000
+	Inceasing pHeader @ index 5 by 0x1000
+	Inceasing pHeader @ index 6 by 0x1000
+	Inceasing pHeader @ index 10 by 0x1000
+	Inceasing pHeader @ index 12 by 0x1000
 [+] Increasing section header addresses if they come after text segment
 [+] Extending section header entry for text section by payload len.
 [+] (16) Updating sections past text section @ addr 0x2000
@@ -80,14 +83,12 @@ Program Headers:
 [+] (28) Updating sections past text section @ addr 0x0
 [+] (29) Updating sections past text section @ addr 0x0
 [+] writing payload into the binary
-[sad0p@arch-deliberate d0zer-payloads]$ ~/go/src/github.com/d0zer/experimental/helloworld
-hello world
 [sad0p@arch-deliberate d0zer-payloads]$ ~/go/src/github.com/d0zer/experimental/helloworld-infected
 Just a test man !!
 hello world
+[sad0p@arch-deliberate d0zer-payloads]$ ~/go/src/github.com/d0zer/experimental/helloworld
+hello world
 [sad0p@arch-deliberate d0zer-payloads]$
-
-
  */
 
 #include <unistd.h>
--- shellcode.py
+++ shellcode.py
@@ -2,17 +2,31 @@
 
 import sys
 import binascii
+from io import StringIO
 
 def main():
-    if len(sys.argv) != 2:
-        print(f"{sys.argv[0]} <file-payload.bin>")
+    if len(sys.argv) < 2:
+        print(f"{sys.argv[0]} <file-payload.bin> <optional-ommit-last-n-bytes>")
         sys.exit(1)
-    bin_file = sys.argv[1]
-    print("\"", end=""); 
-    with open(bin_file, "rb") as fh:
+
+    payload = None
+    with open(sys.argv[1], "rb") as fh:
         payload = binascii.hexlify(fh.read(), " ")
-        print(payload)
+
+    old_stdout = sys.stdout
+    sys.stdout = new_stdout = StringIO()
+
+    print(payload)    
+    
+    sys.stdout = old_stdout
+    shellcode = new_stdout.getvalue().replace("'b", "").replace("'", "").replace("\n", "").split()
+    
+    o_last_bytes = abs(int(sys.argv[2])) * -1 if len(sys.argv) == 3 else len(shellcode) 
+    print("\"", end="")
+    for b in shellcode[:o_last_bytes]:
+        print(f"\\x{b}", end="")
     print("\"")
+
 if __name__ == '__main__':
     main()
 
